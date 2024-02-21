182645050f4a54b09e579b95b0864685a0841237
Vigneshwaran Murugan <145404627+VickyMJackson@users.noreply.github.com>
2024-02-21 23:24:02 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Calculator from './Components/Calculator';
-import Grid from './Components/Grid';
 
 function App() {
   return (
     <div>
      <Calculator />
-     {/* <Grid /> */}
      
     </div>
   );
